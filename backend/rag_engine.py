8358de5a120049fe6fc2ba5b8e469cd952cf13da
Anidu Yakubu Khalid <97510065+AY-Khalid@users.noreply.github.com>
2026-05-17 06:22:37 +0100

    Update import for CharacterTextSplitter module

--- backend/rag_engine.py
+++ backend/rag_engine.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
+# from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain.document_loaders import TextLoader
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
